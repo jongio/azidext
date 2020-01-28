3988eab0d618a9ccd6ce558ba127d73413d31d46
Srikanta <srnagar@microsoft.com>
2020-01-28 13:36:04 -0800

    Clean test code

--- java/com/jongallant/azure/identity/extensions/DefaultAzureServiceBusCredentialTest.java
+++ java/com/jongallant/azure/identity/extensions/DefaultAzureServiceBusCredentialTest.java
@@ -3,17 +3,16 @@ package com.jongallant.azure.identity.extensions;
 import com.azure.identity.DefaultAzureCredential;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
-import com.microsoft.azure.servicebus.security.DefaultAzureServiceBusCredential;
+import com.jongallant.azure.identity.extensions.DefaultAzureServiceBusCredential;
 import org.junit.Test;
 
 public class DefaultAzureServiceBusCredentialTest {
 
     @Test
     public void testDefaultCredential() throws ServiceBusException, InterruptedException {
-        DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredentialBuilder().build();
         TopicClient topicClient = new TopicClient("jongsb2", "srnagartopic",
             new ClientSettings(new DefaultAzureServiceBusCredential()));
-
         topicClient.send(new Message("hello"));
+        topicClient.close();
     }
 }
