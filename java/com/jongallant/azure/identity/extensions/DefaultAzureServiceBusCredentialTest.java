4ecde2e11749ac090d33fef8a4f9c600bab3d3de
Srikanta <51379715+srnagar@users.noreply.github.com>
2020-01-28 13:27:28 -0800

    Java support for DefaultAzureCredential in Service Bus (#3)
    * Java support for DefaultAzureCredential in Service Bus
    
    * Moving to a different namespace

---
+++ java/com/jongallant/azure/identity/extensions/DefaultAzureServiceBusCredentialTest.java
@@ -0,0 +1,19 @@
+package com.jongallant.azure.identity.extensions;
+
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.microsoft.azure.servicebus.primitives.ServiceBusException;
+import com.microsoft.azure.servicebus.security.DefaultAzureServiceBusCredential;
+import org.junit.Test;
+
+public class DefaultAzureServiceBusCredentialTest {
+
+    @Test
+    public void testDefaultCredential() throws ServiceBusException, InterruptedException {
+        DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredentialBuilder().build();
+        TopicClient topicClient = new TopicClient("jongsb2", "srnagartopic",
+            new ClientSettings(new DefaultAzureServiceBusCredential()));
+
+        topicClient.send(new Message("hello"));
+    }
+}
