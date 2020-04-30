7e1279aefa0e6d643eac343bdb5c5dff1218d601
Luyunmt <2534523917@qq.com>
2020-04-30 16:59:08 +0800

    add js defaultAzureCredentialAdapter and test

--- js/defaultAzureCredentialAdapter.ts
+++ js/defaultAzureCredentialAdapter.ts
@@ -10,31 +10,29 @@ const DEFAULT_AUTHORIZATION_SCHEME = "Bearer";
  * use with legacy Azure SDKs that accept {@link ServiceClientCredentials} family of credentials for authentication.
  */
 export class DefaultAzureCredentialAdapter implements ServiceClientCredentials {
-    private defaultAzureCredential: DefaultAzureCredential;
-    private scopes: string | string[];
-    constructor(
-        defaultAzureCredential = new DefaultAzureCredential(),
-        scopes: string | string[] = "https://management.azure.com/.default",
-      ) { 
-        this.defaultAzureCredential = defaultAzureCredential;
-        this.scopes = scopes;
-      }
+  private defaultAzureCredential: DefaultAzureCredential;
+  private scopes: string | string[];
+  constructor(
+      defaultAzureCredential = new DefaultAzureCredential(),
+      scopes: string | string[] = "https://management.azure.com/.default",
+    ) { 
+      this.defaultAzureCredential = defaultAzureCredential;
+      this.scopes = scopes;
+    }
  
-      public async getToken(): Promise<TokenResponse>{
-
-        const accessToken = (await this.defaultAzureCredential.getToken(this.scopes));
-        const result: TokenResponse = {
-          accessToken: accessToken.token,
-          tokenType: DEFAULT_AUTHORIZATION_SCHEME,
-          expiresOn: accessToken.expiresOnTimestamp,
-        };
-        return result;
-      }
-
-      public async signRequest(webResource: WebResource) {
-        const tokenResponse = await this.getToken();
-        webResource.headers.set(MSRestConstants.HeaderConstants.AUTHORIZATION, `${tokenResponse.tokenType} ${tokenResponse.accessToken}`);
-        return Promise.resolve(webResource);
-      }
+  public async getToken(): Promise<TokenResponse>{
+    const accessToken = (await this.defaultAzureCredential.getToken(this.scopes));
+    const result: TokenResponse = {
+      accessToken: accessToken.token,
+      tokenType: DEFAULT_AUTHORIZATION_SCHEME,
+      expiresOn: accessToken.expiresOnTimestamp,
+    };
+    return result;
   }
-  
+
+  public async signRequest(webResource: WebResource) {
+    const tokenResponse = await this.getToken();
+    webResource.headers.set(MSRestConstants.HeaderConstants.AUTHORIZATION, `${tokenResponse.tokenType} ${tokenResponse.accessToken}`);
+    return Promise.resolve(webResource);
+  }
+}
