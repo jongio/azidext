d27914cab1817a921d72467bf3fc02e4b62c3099
Luyunmt <2534523917@qq.com>
2020-06-09 09:59:44 +0800

    change according to comments

--- js/defaultAzureCredentialAdapter.spec.ts
+++ js/defaultAzureCredentialAdapter.spec.ts
@@ -1,17 +1,28 @@
 import assert from "assert";
+import * as dotenv from "dotenv";
+import { isNode } from "@azure/core-http";
 import { KeyVaultManagementClient } from "@azure/arm-keyvault";
 import { DefaultAzureCredentialAdapter } from "./defaultAzureCredentialAdapter";
 
-const subscriptionId = "AZUER_SUBSCRIPTION_ID";
+if (isNode) {
+  dotenv.config();
+}
+
+const subscriptionId = process.env.SUBSCRIPTION_ID;
 
 describe("DefaultAzureCredentialAdapter", function() {
   it("get exsit key vault ", async () => {
-    const cred = new DefaultAzureCredentialAdapter();
-    const client = new KeyVaultManagementClient(cred, subscriptionId);
-    const resourceGroupName = "AZURE_RESOURCE_GROUP_NAME";
-    const vaultName = "AZURE_EXSIT_KEY_VAULT_NAME";
-    client.vaults.get(resourceGroupName, vaultName).then((result) => {
-     assert.equal(result.name, vaultName)
-    });
-  });
+    try {
+      const cred = new DefaultAzureCredentialAdapter();
+      const client = new KeyVaultManagementClient(cred, subscriptionId);
+      const resourceGroupName = process.env.AZURE_RESOURCE_GROUP_NAME;
+      const vaultName = process.env.AZURE_EXSIT_KEY_VAULT_NAME;
+      await client.vaults.get(resourceGroupName, vaultName).then((result) => {
+        console.log("The result is:");
+        console.log(result);
+      })
+    } catch (err) {
+      throw err
+    }
+  })
 });
