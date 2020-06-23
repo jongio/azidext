21e7f31e26b44fee4d478b478538600d29785704
Luyunmt <2534523917@qq.com>
2020-06-23 11:01:00 +0800

    change test

--- js/azureIdentityCredentialAdapter.spec.ts
+++ js/azureIdentityCredentialAdapter.spec.ts
@@ -1,4 +1,4 @@
-import assert from "assert";
+import * as assert from "assert";
 import * as dotenv from "dotenv";
 import { ResourceManagementClient } from "@azure/arm-resources";
 import { AzureIdentityCredentialAdapter } from "./azureIdentityCredentialAdapter ";
@@ -9,13 +9,11 @@ const subscriptionId = process.env.AZURE_SUBSCRIPTION_ID;
 
 describe("AzureIdentityCredentialAdapter ", function() {
   it("create resoucegroup and delete it", async () => {
-      const cred = new AzureIdentityCredentialAdapter ();
+      const cred = new AzureIdentityCredentialAdapter();
       const client = new ResourceManagementClient(cred, subscriptionId);
-      const name = "azidextrg" + (Math.floor(Math.random()*89999999)+10000000);    
-      const result = await client.resourceGroups.createOrUpdate(name, { location: 'westus', name : name});
-      assert.equal(result.name,name);
+      const name = "azidextrg" + (Math.floor(Math.random()* 89999999) + 10000000);    
+      const result = await client.resourceGroups.createOrUpdate(name, { location: 'westus', name : name });
+      assert.equal(result.name, name);
       await (await client.resourceGroups.beginDeleteMethod(name)).pollUntilFinished;  
-  })
+  });
 });
-
-  
